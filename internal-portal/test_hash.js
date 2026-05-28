1be5f9f8b1ff3573f6a836beec997a4f0e3c52e6
jasetsha <jasetsha@gmail.com>
2026-05-28 21:24:21 +0200

    Clean up temporary test files

--- internal-portal/test_hash.js
+++
@@ -1,5 +0,0 @@
-const hex = '2432622431302453666b6a734f526d626b48313052397035747535674f6344614b624e4178663431393053417a4334594d5149562e52506462614969';
-const hash = Buffer.from(hex, 'hex').toString('utf8');
-console.log('Decoded hash:', hash);
-console.log('Starts with $2b$10$:', hash.startsWith('$2b$10$') ? 'YES' : 'NO');
-console.log('Length:', hash.length);
